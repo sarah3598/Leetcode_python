3372099875f2a06855a96f17d66fc2c370e5db9e
sarah3598 <yuanshuo98@sina.com>
2019-04-18 21:00:18 +0800

    001

--- Leetcode_python/001twoSum.py
+++ Leetcode_python/001twoSum.py
@@ -1,13 +1,23 @@
-def twoSum(nums, target):
-    res=[]
-    for i in range(0,len(nums)-1):
-        p1 = nums[i]
-        for j in range(i+1,len(nums)):
-            if nums[j]==target-p1:
-                res.append(i)
-                res.append(j)
-                return  res
-            continue
+#####solution01#####
+# def twoSum(nums, target):
+#     res=[]
+#     for i in range(0,len(nums)-1):
+#         p1 = nums[i]
+#         for j in range(i+1,len(nums)):
+#             if nums[j]==target-p1:
+#                 res.append(i)
+#                 res.append(j)
+#                 return  res
+#             continue
+#
+####solution02######
+def twoSum(nums,target):
+    dic={}
+    for i in range(len(nums)):
+        if nums[i] in dic:
+            return [dic[nums[i]],i]
+        else:
+            dic[target-nums[i]]=i
 if __name__=='__main__':
     list=[2,7,11,15]
     t=9
