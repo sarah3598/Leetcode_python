122236cbcfc91ca8b7ab4bff3fac9beef2fb1a46
sarah3598 <yuanshuo98@sina.com>
2019-04-20 12:31:53 +0800

    两个有序列表合并输出第k个数

---
+++ Leetcode_python/findK.py
@@ -0,0 +1,25 @@
+#coding=utf-8
+import sys
+def printK(l1,l2,k):
+    #l=len(l1)
+    l3=[]
+    i=0
+    j=0
+    while i+j<=k-1:
+        if l1[i]<l2[j]:
+            l3.append(l1[i])
+            i=i+1
+        elif l1[i]>l2[j]:
+            l3.append(l2[j])
+            j=j+1
+        else:
+            l3.append(l1[i])
+            l3.append(l2[j])
+            i=i+1
+            j=j+1
+    return l3,l3[k-1]
+if __name__=='__main__':
+    a=[3,7,9]
+    b=[0,3,11]
+    k=3
+    print(printK(a,b,k))
