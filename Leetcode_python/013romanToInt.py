d2cfbf2c7e4a89a0f38f4406afa35b751300dc95
sarah3598 <yuanshuo98@sina.com>
2019-04-23 22:27:41 +0800

    013

--- Leetcode_python/013romanToInt.py
+++ Leetcode_python/013romanToInt.py
@@ -1,3 +1,4 @@
+#####solution1####faster####
 def romanToInt(s):
     d={
         'I':1,
@@ -18,9 +19,30 @@ def romanToInt(s):
         last = current
         i += 1
     return count
-if __name__=='__main__':
-    m="MCMXCIV"
-    print(romanToInt(m))
+# ########solution2########
+# def romanToInt(s):
+#     d = {'I': 1,
+#          'V': 5,
+#          'X': 10,
+#          'L': 50,
+#          'C': 100,
+#          'D': 500,
+#          'M': 1000
+#          }
+#     res = 0
+#     if len(s) < 2:
+#         return d[s[0]]
+#     res = res + d[s[0]]
+#     for i in range(1,len(s)):
+#         res = res + d[s[i]]
+#         if d[s[i]] > d[s[i - 1]]:
+#             res = res - 2 * d[s[i-1]]
+#     return res
+#
+#
+# if __name__=='__main__':
+#     m="MCMXCIV"
+#     print(romanToInt(m))
 
 
 
